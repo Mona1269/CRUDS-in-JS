9faab875dfe1ebd36ef7bac15269c1e85ae3cc2e
Mona Ashraf <84921194+Mona1269@users.noreply.github.com>
2023-11-21 14:23:34 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,1 @@
-let NameInput=document.getElementById('Name');
-let PriceInput=document.getElementById('Price');
-let CatInput=document.getElementById('Cat');
-let DesInput=document.getElementById('Des');
-console.log(Name , Price , Cat , Des);
-productsContainer = [];
-function addProduct() {
-    let product = {
-Name:NameInput.value ,
-Price:PriceInput.value ,
-Cat:CatInput.value ,
-Des:DesInput.value
 
-}
-pr
-}
